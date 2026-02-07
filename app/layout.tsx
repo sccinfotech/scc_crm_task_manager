675061b23dfdff0c7df5c170b45df397f59ee312
sarikascc <sarika.scc@gmail.com>
2026-02-07 11:47:44 +0530

    Always set on Light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light" suppressHydrationWarning>
       <body
         className={`${poppins.variable} ${openSans.variable} font-sans antialiased`}
       >
